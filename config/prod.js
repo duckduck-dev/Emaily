ec199649d51a2e745ccefac8bc285e19962160b4
soundarsurya45 <duckduck-dev>
2020-07-28 20:07:04 +0530

    finished auth flow

--- config/prod.js
+++ config/prod.js
@@ -1,11 +1,10 @@
 const { googleClientID } = require("./dev");
 
 module.exports = {
-     /* productionmongoURI: 'mongodb+srv://production:RSxZzSLyVE8uhYBn@cluster0.uhwxd.mongodb.net/prodDb?retryWrites=true&w=majority',
-      prodgoogleClientID: '327027291962-dtbpfjrq46qnjpqp4qi60kj03nqlirvi.apps.googleusercontent.com',
-      prodgoogleClientSecret: 'YWRjnRnaGkAShyMHz0oMzPqE',
+     /* mongoURI: 'mongodb+srv://production:RSxZzSLyVE8uhYBn@cluster0.uhwxd.mongodb.net/prodDb?retryWrites=true&w=majority',
+      googleClientID: '327027291962-dtbpfjrq46qnjpqp4qi60kj03nqlirvi.apps.googleusercontent.com',
+      googleClientSecret: 'YWRjnRnaGkAShyMHz0oMzPqE',
       cookieKey: 'urwytwrohfjdsvndancjwqhfuowncjad'*/
-
 
       googleClientID : process.env.GOOGLE_CLIENT_ID,
       googleClientSecret : process.env.GOOGLE_CLIENT_SECRET,
