3b16344a3c6642e9956eb4c86ea4538aa9e3b1b9
surya <bittergourd76@gmail.com>
2020-09-29 16:57:24 +0530

    Updated surveyTemplates

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,11 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    app.post( '/api/surveys', requireLogin, requireCredits, (req, res) => {
+    app.get('api/surveys/thanks', (req, res) => {
+        res.send("Thanks for voting!");
+    } );
+
+    app.post( '/api/surveys', requireLogin, requireCredits, async (req, res) => {
             const { title, subject, body, recipients } = req.body;
             
             //spliting the ',' separated array of recipients and returning them as separate objects.
@@ -23,6 +27,15 @@ module.exports = app => {
             } );
             
             const mailer = new Mailer(survey, surveyTemplate(survey));
-            mailer.send();
+            try{
+            await mailer.send();
+            await survey.save();
+            req.user.credits -= 1;
+            const user = await req.user.save();
+            res.send(user);
+            }
+            catch(e){
+                res.status(422).send(e);
+            }
     } );
 };
--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,24 @@
+const keys = require('../../config/keys');
+
 module.exports = survey => {
-    return '<div>'+survey.body+'</div>';
+    return `
+        <html>
+            <body style="background: skyblue;">
+                <div style="text-align: center;">
+                    <h3>Hi.Hey.Hello.Hola</h3>
+                    <p>Please answer the following questions:</p>
+                    <p>${survey.body}</p>
+                    <div>
+                        <a href="${keys.redirectDomain}/api/surveys/thanks">Yes</a>
+                    </div>
+                    <div>
+                        <a href="${keys.redirectDomain}/api/surveys/thanks">No</a>
+                    </div>
+                    <div>
+                    Made with <span style="color:red;">&hearts;</span> by ThunderMail.
+                    </div>
+                </div>
+            </body>
+        </html>
+    `;
 };
